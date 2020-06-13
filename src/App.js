24d669ea448f7f96bde748804369e744816ba1fd
ranawaqas92 <m.r.waqas@hotmail.com>
2020-06-14 04:43:06 +0500

    remove Logo lib

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import ChildApp from './ChildApp'
 //import childApp from './childApp.js';
